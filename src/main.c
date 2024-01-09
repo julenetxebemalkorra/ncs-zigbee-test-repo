82c4f8d0566a8b763f6b01ec7235b378c2b4035d
IEDELECTRONICS\jetxeberria <jetxeberria@iedcompany.com>
2024-01-09 12:45:34 +0100

    clean up and write some notes

--- src/main.c
+++ src/main.c
@@ -38,6 +38,7 @@
 /* Button used to enter the Identify mode. */
 #define IDENTIFY_MODE_BUTTON             1
 
+/* Flag  used to print zigbee info once the device joins a network. */
 #define PRINT_ZIGBEE_INFO             	ZB_TRUE
 
 /* 1000 msec = 1 sec */
@@ -46,15 +47,18 @@
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
 
-#define MSG_SIZE 32
-
+/* Default tick to consider a modbus frame completed*/
 #define DEFAULT_TICKS_TO_CONSIDER_FRAME_COMPLETED 21 // At 19200 bps, 4T = 2083 us --> 21 ticks of 100 us
 
+/*UART Modbus and zigbee buffer size definitions*/
 #define UART_RX_BUFFER_SIZE              255 //253 bytes + CRC (2 bytes) = 255
-#define UART_TX_BUFFER_SIZE              255 //253 bytes + CRC (2 bytes) = 255
-#define MODBUS_MIN_RX_LENGTH             8
-#define MAX_PAYLOAD_SIZE 				 82 // Maximum payload size that can be sent at once
+#define MODBUS_MIN_RX_LENGTH             8   // if the messagge has 8 bytes we consider it a modbus frame
+#define MAX_ZIGBEE_PAYLOAD_SIZE 		 82 // Maximum payload size that can be sent at once via zigbee
 
+/*Zigbee NWK configuration
+
+Note: This part was added to wireshark encription issues
+*/
 #define ZB_ZGP_DEFAULT_SHARED_SECURITY_KEY_TYPE ZB_ZGP_SEC_KEY_TYPE_NWK
 #define ZB_ZGP_DEFAULT_SECURITY_LEVEL ZB_ZGP_SEC_LEVEL_FULL_WITH_ENC
 #define ZB_STANDARD_TC_KEY { 0x81, 0x42, < rest of key > };
@@ -73,12 +77,8 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 // Get a reference to the TIMER1 instance
 static const nrfx_timer_t my_timer = NRFX_TIMER_INSTANCE(1);
 
-static char rx_buf[UART_RX_BUFFER_SIZE];
-static int rx_buf_pos;
-
+/* UART configuration variables*/
 static volatile char UART_rx_buffer[UART_RX_BUFFER_SIZE];
-static volatile char UART_tx_buffer[UART_TX_BUFFER_SIZE];
-
 static volatile bool b_UART_receiving_frame;
 static volatile uint16_t UART_ticks_since_last_byte;
 static uint16_t UART_ticks_to_consider_frame_completed;
@@ -88,13 +88,17 @@ static volatile uint16_t UART_rx_buffer_index_max;
 static volatile int counter = 0;
 static volatile size_t offset = 0;
 
-
+/*  This macro normally must be used after including <zephyr/logging/log.h> to
+ * complete the initialization of the module. 
+ 
+ Note: it may be removed
+ */
 LOG_MODULE_REGISTER(app, LOG_LEVEL_NONE);
 
-// boolean flag for detecting modbus request received from gateway
-bool bModbusRequestReceived = false;
-bool bModbusRequestReady = false;
-bool bConnected = false;
+// boolean flags for detecting modbus request handling
+bool b_Modbus_Request_Received_via_Zigbee = false;
+bool b_Modbus_Ready_to_send = false;
+bool b_Zigbe_Connected = false;
 
 /* Main application customizable context.
  * Stores all settings and static values.
@@ -107,29 +111,33 @@ struct zb_device_ctx {
 /* Zigbee device application context storage. */
 static struct zb_device_ctx dev_ctx;
 
+/*Declare attribute list for Identify cluster*/
 ZB_ZCL_DECLARE_IDENTIFY_ATTRIB_LIST(
 	identify_attr_list,
 	&dev_ctx.identify_attr.identify_time);
 
+/*Declare Basic attribute list*/
 ZB_ZCL_DECLARE_BASIC_ATTRIB_LIST(
 	basic_attr_list,
 	&dev_ctx.basic_attr.zcl_version,
 	&dev_ctx.basic_attr.power_source);
 
+/*Declare cluster list for Range extender device*/
 ZB_DECLARE_RANGE_EXTENDER_CLUSTER_LIST(
 	app_template_clusters,
 	basic_attr_list,
 	identify_attr_list);
 
+/*Declare endpoint for Range extender device*/
 ZB_DECLARE_RANGE_EXTENDER_EP(
 	app_template_ep,
 	APP_TEMPLATE_ENDPOINT,
 	app_template_clusters);
 
+/*Declare application's device context for single-endpoint device*/
 ZBOSS_DECLARE_DEVICE_CTX_1_EP(
 	app_template_ctx,
 	app_template_ep);
-
 
 /**@brief Function for initializing all clusters attributes. */
 static void app_clusters_attr_init(void)
@@ -152,7 +160,7 @@ static void app_clusters_attr_init(void)
 /* Returns: None                                                              */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
-void UART_init(void)
+void UART_conf_init(void)
 { 
     b_UART_receiving_frame = false;
     UART_ticks_since_last_byte = 0;
@@ -248,13 +256,12 @@ zb_uint8_t data_indication(zb_bufid_t bufid)
 				//if ((sizeOfPayload == 8) && (pointerToBeginOfBuffer[0] == 0xC2))
 				if ((sizeOfPayload == MODBUS_MIN_RX_LENGTH) && (pointerToBeginOfBuffer[0] == 0x11))
 				{
-					bModbusRequestReceived = true;
+					b_Modbus_Request_Received_via_Zigbee = true;
 					printk("bModbusRequestReceived via zigbee and send via UART \n");
 
 					for (uint8_t i = 0; i < sizeOfPayload; i++)
 					{
 						send_uart(pointerToBeginOfBuffer[i]);
-						//printk("%c- ", UART_tx_buffer[i]);
 					}
 
 				}
@@ -307,11 +314,20 @@ void zboss_signal_handler(zb_bufid_t bufid)
 	}
 }
 
-
-void send_user_payload(zb_uint8_t *outputPayload,size_t chunk_size)
+//------------------------------------------------------------------------------
+/**@brief Zigbee helper function  to send user payload
+ *
+ * @param[in]   outputPayload   Reference to the user payload 
+ * @param[in]   chunk_size   payload size to send 
+ *
+ */
+void send_user_payload(zb_uint8_t *outputPayload ,size_t chunk_size)
 {
+	    /*Allocate OUT buffer of the default size.*/
 		zb_bufid_t bufid;
 		bufid = zb_buf_get_out();
+
+		/*destination address: for the first test the network coordinator will be the destination */
 		zb_addr_u dst_addr;
 		dst_addr.addr_short = 0x0000;
 		
@@ -346,20 +362,36 @@ void send_user_payload(zb_uint8_t *outputPayload,size_t chunk_size)
 			    				 outputPayload,
 				    			 chunk_size);
 
+	/*Free packet buffer and put it into free list after payload is sent*/
 	if (bufid) 
 	{
 	    zb_buf_free(bufid);
 	}
 
+/*
+  Note: This code includes a one-second delay between Zigbee package transmissions for stability.
+
+  To adjust the delay time:
+  - Locate the 'sleep' function.
+  - Modify the argument value in 'sleep' for a different delay duration.
+  - Test the code after changing the delay to ensure proper functionality.
+  
+  Caution: Be mindful when altering the delay to prevent network congestion.
+*/
 	k_msleep(SLEEP_TIME_MS);
 
 }
 
+//------------------------------------------------------------------------------
+/**@brief Function to send the received modbus answer. Logic to split the messages in MAX_ZIGBEE_PAYLOAD_SIZE
+ *
+ *
+ */
 void send_zigbee_modbus_answer(void)
 {
 
 	//zb_uint8_t outputPayload[255] = {0xC2, 0x04, 0x04, 0x00, 0x4A, 0x75, 0x6C, 0x65, 0x6E, 0x4A, 0x75, 0x6C, 0x65, 0x6E};
-	zb_uint8_t outputPayload[MAX_PAYLOAD_SIZE];
+	zb_uint8_t outputPayload[MAX_ZIGBEE_PAYLOAD_SIZE];
 /*
 	printk("send_zigbee_modbus_answer Size of answer send via zigbee is %d bytes \n", UART_rx_buffer_index_max);
 
@@ -370,6 +402,7 @@ void send_zigbee_modbus_answer(void)
 
 	printk("UART_rx_buffer \n ");
 */
+    // Manipulating the received buffer before processing this is to test big data packages
 	for (uint8_t i = 0; i <= (UART_rx_buffer_index_max * 20); i = i+5)
 	{
 		UART_rx_buffer[i] = UART_rx_buffer[0];
@@ -390,9 +423,10 @@ void send_zigbee_modbus_answer(void)
 */
 	size_t remaining_length = UART_rx_buffer_index_max;
 
+    // Transmit the modified payload in chunks via Zigbee
 	while(remaining_length > 0) 
 	{
-    	size_t chunk_size = MIN(remaining_length, MAX_PAYLOAD_SIZE);
+    	size_t chunk_size = MIN(remaining_length, MAX_ZIGBEE_PAYLOAD_SIZE);
 
 		printk("chunk_size %d bytes \n", chunk_size);
 		printk("remaining_length %d bytes \n", remaining_length);
@@ -406,12 +440,13 @@ void send_zigbee_modbus_answer(void)
 */
 		send_user_payload(&outputPayload, chunk_size);
 
-		bModbusRequestReceived = false;
+        // Reset flags and counters for the next chunk
+		b_Modbus_Request_Received_via_Zigbee = false;
 		counter = 0;
+
 		// Update offset and remaining length for the next chunk
     	offset += chunk_size;
     	remaining_length -= chunk_size;
-		//printk("RESPONDIDA9");
 	}
 
 }
@@ -421,15 +456,15 @@ void send_zigbee_modbus_answer(void)
 //		 and if you need to access one of these from the timer callback it is necessary to use something like a k_work item to move execution out of the interrupt context. 
 void timer1_event_handler(nrf_timer_event_t event_type, void * p_context)
 {
+
 	switch(event_type) {
 		case NRF_TIMER_EVENT_COMPARE0:
-			// Do your work here
 			//if (!bModbusRequestReceived) printk("Timer 1 callback. Counter = %d bModbusRequestReceived %d\n", counter, bModbusRequestReceived);
-			if (bModbusRequestReceived)
+			if (b_Modbus_Request_Received_via_Zigbee)
 			{
 				//printk("Timer 1 callback. Counter = %d bModbusRequestReceived %d\n", counter, bModbusRequestReceived);
 
-				get_uart(rx_buf);
+				get_uart();
 
 				counter++;
 
@@ -439,14 +474,14 @@ void timer1_event_handler(nrf_timer_event_t event_type, void * p_context)
     			    {
     			        b_UART_receiving_frame = false; 
 						UART_ticks_since_last_byte = 0;
-						bModbusRequestReady = true;
+						b_Modbus_Ready_to_send = true;
 						counter = 0;
     			    }
     			}
 			}
-			if ((bModbusRequestReceived == true) && (counter > 100))
+			if ((b_Modbus_Request_Received_via_Zigbee == true) && (counter > 100))
 			{
-				bModbusRequestReceived = false;
+				b_Modbus_Request_Received_via_Zigbee = false;
 				counter = 0;
 			}
 			break;
@@ -454,6 +489,15 @@ void timer1_event_handler(nrf_timer_event_t event_type, void * p_context)
 		default:
 			break;
 	}
+}
+
+// Function for scheduling repeated callbacks from TIMER1
+static void timer1_repeated_timer_start(uint32_t timeout_us)
+{
+	nrfx_timer_enable(&my_timer);
+
+	nrfx_timer_extended_compare(&my_timer, NRF_TIMER_CC_CHANNEL0, timeout_us, 
+                                NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK, true);
 }
 
 // Function for initializing the TIMER1 peripheral using the nrfx driver
@@ -470,15 +514,10 @@ static void timer1_init(void)
 
 	IRQ_DIRECT_CONNECT(TIMER1_IRQn, 0, nrfx_timer_1_irq_handler, 0);
 	irq_enable(TIMER1_IRQn);
-}
 
-// Function for scheduling repeated callbacks from TIMER1
-static void timer1_repeated_timer_start(uint32_t timeout_us)
-{
-	nrfx_timer_enable(&my_timer);
-
-	nrfx_timer_extended_compare(&my_timer, NRF_TIMER_CC_CHANNEL0, timeout_us, 
-                                NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK, true);
+	// Setup TIMER1 to generate callbacks every second 1000000
+	// Setup TIMER1 to generate callbacks every 100ms 100000
+	timer1_repeated_timer_start(100000);
 }
 
 /*
@@ -492,15 +531,17 @@ void send_uart(uint8_t out_uint8)
 /*
  * Print a null-terminated string character by character to the UART interface
  */
-void get_uart(char *buf)
+void get_uart()
 {
 	int ret;
+	/* temp array for get_uart function*/
+	char rx_buf[UART_RX_BUFFER_SIZE];
 
-	ret = uart_poll_in(dev_uart, &buf);
+	ret = uart_poll_in(dev_uart, &rx_buf);
 
 	if(ret == 0) 
 	{
-		printk("UART char arrived %c \n", buf);     
+		printk("UART char arrived %c \n", rx_buf);     
 		//printk("char arrived\n");
 		if( b_UART_receiving_frame )
     	{
@@ -513,7 +554,7 @@ void get_uart(char *buf)
     	        }
     	        else
     	        {                       
-    	            UART_rx_buffer[UART_rx_buffer_index] = buf;     
+    	            UART_rx_buffer[UART_rx_buffer_index] = rx_buf;     
 					printk("UART char arrived %c index: %d \n",UART_rx_buffer[UART_rx_buffer_index], (UART_rx_buffer_index));     
 					UART_rx_buffer_index_max = UART_rx_buffer_index;        
     	            UART_rx_buffer_index++;
@@ -526,7 +567,7 @@ void get_uart(char *buf)
 		{
     	    b_UART_receiving_frame = true;
     	    b_UART_overflow = false;
-    	    UART_rx_buffer[0] = buf;
+    	    UART_rx_buffer[0] = rx_buf;
 			printk("UART char arrived %c index: %d \n",UART_rx_buffer[UART_rx_buffer_index], (UART_rx_buffer_index));             
     	    UART_rx_buffer_index = 1;
 			UART_rx_buffer_index_max = UART_rx_buffer_index;        
@@ -563,7 +604,7 @@ static void app_uart_init(void)
 		return 0;
 	}
 	
-	uart_rx_enable(dev_uart, UART_rx_buffer, MSG_SIZE, SYS_FOREVER_US );
+	uart_rx_enable(dev_uart, UART_rx_buffer, UART_RX_BUFFER_SIZE, SYS_FOREVER_US );
 }
 
 
@@ -582,34 +623,8 @@ static void gpio_init(void)
 	}
 }
 
-
-int main(void)
+void zigbee_configuration()
 {
-	zb_ieee_addr_t zb_long_address;
-	zb_ext_pan_id_t zb_ext_pan_id;
-	zb_nwk_device_type_t zb_role;
-	zb_uint8_t zb_channel;
-	zb_uint16_t zb_shrot_addr;
-	int infit_info_flag = PRINT_ZIGBEE_INFO;
-	zb_ret_t zb_err_code;
-    zb_ieee_addr_t ieee_addr;
-	int blink_status = 0;
-	
-	char tx_buf[MSG_SIZE];
-
-	/* Initialize */
-	app_uart_init();
-
-	// Initialize TIMER1
-	timer1_init();
-	
-	UART_init();
-
-	gpio_init();
-	
-	// Setup TIMER1 to generate callbacks every second
-	timer1_repeated_timer_start(100000);
-
 	zb_set_nvram_erase_at_start(ZB_TRUE);
 
 	/* Register device context (endpoints). */
@@ -628,71 +643,102 @@ int main(void)
 	zb_bdb_set_legacy_device_support(ZB_TRUE);
 
 	app_clusters_attr_init();
+}
+
+void diagnostic_toogle_pin()
+{
+	int ret = gpio_pin_toggle_dt(&led);
+	if (ret < 0) {
+		return;
+	}
+	k_msleep(SLEEP_TIME_MS);
+}
+
+void diagnostic_zigbee_info()
+{
+	zb_ieee_addr_t zb_long_address;
+	zb_ext_pan_id_t zb_ext_pan_id;
+	zb_nwk_device_type_t zb_role;
+	zb_uint8_t zb_channel;
+	zb_uint16_t zb_shrot_addr;
+	int infit_info_flag = PRINT_ZIGBEE_INFO;
+	zb_ret_t zb_err_code;
+    zb_ieee_addr_t ieee_addr;
+
+	if(zb_zdo_joined() && infit_info_flag == ZB_TRUE)
+	{
+		infit_info_flag = ZB_FALSE;
+		b_Zigbe_Connected = true;
+		printk("Zigbee application joined the network: bellow some info: \n");
+		zb_get_long_address(zb_long_address);
+		// Log Long Address
+		printk("zigbee long addr: ");
+		for (int i = 7; i >= 0; i--) {
+		    printk("%02x", zb_long_address[i]);
+		}
+		printk("\n");
+		zb_shrot_addr = zb_get_short_address();
+		printk("zigbee shrot addr:  0x%x\n", zb_shrot_addr);
+		zb_get_extended_pan_id(zb_ext_pan_id);	
+		// Log Extended PAN ID
+		printk("zigbee extended pan id: ");
+		for (int i = 7; i >= 0; i--) {
+		    printk("%02x", zb_ext_pan_id[i]);
+		}
+		printk("\n");
+		switch(zb_get_network_role())
+		{
+		case 0:
+			printk("zigbee role coordinator\n");
+			break;
+		case 1:
+			printk("zigbee role router\n");
+			break;
+		case 2:
+			printk("zigbee role end device\n");
+			break;
+		default:
+			printk("Zigbee role NOT found \n");
+			break;
+		}
+		
+	zb_channel = zb_get_current_channel();	
+	printk("zigbee channel: %d \n", zb_channel);
+	}
+}
+
+int main(void)
+{
+	/* Initialize UART*/
+	app_uart_init();
+
+	// Initialize TIMER1
+	timer1_init();
+	
+	UART_conf_init();
+
+	gpio_init();
+
+	zigbee_configuration();
 
 	/* Start Zigbee default thread */
 	zigbee_enable();
 
 	while(1)
 	{		
-		int ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return;
-		}
-		k_msleep(SLEEP_TIME_MS);
 
-		if(zb_zdo_joined() && infit_info_flag == ZB_TRUE)
-		{
-			infit_info_flag = ZB_FALSE;
-			bConnected = true;
-			printk("Zigbee application joined the network: bellow some info: \n");
+		diagnostic_toogle_pin();
 
-			zb_get_long_address(zb_long_address);
-			// Log Long Address
-			printk("zigbee long addr: ");
-			for (int i = 7; i >= 0; i--) {
-			    printk("%02x", zb_long_address[i]);
-			}
-			printk("\n");
+		diagnostic_zigbee_info();
 
-			zb_shrot_addr = zb_get_short_address();
-			printk("zigbee shrot addr:  0x%x\n", zb_shrot_addr);
-
-			zb_get_extended_pan_id(zb_ext_pan_id);	
-			// Log Extended PAN ID
-			printk("zigbee extended pan id: ");
-			for (int i = 7; i >= 0; i--) {
-			    printk("%02x", zb_ext_pan_id[i]);
-			}
-			printk("\n");
-
-			switch(zb_get_network_role())
-			{
-			case 0:
-				printk("zigbee role coordinator\n");
-				break;
-			case 1:
-				printk("zigbee role router\n");
-				break;
-			case 2:
-				printk("zigbee role end device\n");
-				break;
-			default:
-				printk("Zigbee role NOT found \n");
-				break;
-			}
-			
-		zb_channel = zb_get_current_channel();	
-		printk("zigbee channel: %d \n", zb_channel);
-
-		}
-
-		if(bModbusRequestReady)
+		if(b_Modbus_Ready_to_send)
 		{
 			send_zigbee_modbus_answer();
-			bModbusRequestReady = false;
+			b_Modbus_Ready_to_send = false;
 			UART_rx_buffer_index = 0;
 			offset=0;
 		}
 	}
+
 	return 0;
 }
