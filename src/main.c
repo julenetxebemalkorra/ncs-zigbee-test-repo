0f4c178e963fb1f6b73776622596a81ab8d4d885
IEDJiriarte <jiriarte@iedcompany.com>
2024-02-29 19:30:37 +0100

    He modificado la informacion que muestra el log para ayudar a entender el porqué de la latencia tan alta que se observa

--- src/main.c
+++ src/main.c
@@ -62,10 +62,12 @@
 
 uint32_t reset_cause; // Bit register containg the last reset cause.
 
-uint16_t correct_RF_packets_received_counter = 0;
+uint16_t aps_frames_received_total_counter = 0;
+uint16_t aps_frames_received_binary_cluster_counter = 0;
+uint16_t aps_frames_received_commissioning_cluster_counter = 0;
+
+uint16_t tcu_uart_frames_transmitted_counter = 0;
 uint16_t tcu_uart_frames_received_counter = 0;
-uint16_t correct_RF_packets_received_counter_old = 0;
-uint16_t tcu_uart_frames_received_counter_old = 0;
 
 //
 static const zb_uint8_t ext_pan_id[8] = {0x99, 0x99, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
@@ -88,8 +90,6 @@ volatile uint16_t UART_rx_buffer_index;
 volatile uint16_t UART_rx_buffer_index_max;
 
 static volatile size_t offset = 0;
-
-static volatile	zb_uint8_t *pointerToBeginOfBuffer;
 
 /* Zigbee messagge info*/
 
@@ -187,57 +187,88 @@ void get_reset_reason(void)
 }
 
 //------------------------------------------------------------------------------
-/**@brief Callback function to read status of last APS frame transmission.
+/**@brief Callback function executed when APS frame transmission is completed.
  *
- * @param[in]   bufid   Reference to the Zigbee stack buffer used to transmit the APS frame
+ * @details A log message showing APS, NWK and MAC counters is printed.
+ *          The buffer is released.
+ *
+ * @param   bufid   Reference to the Zigbee stack buffer used to transmit the APS frame
+ *
+ * @note The callback function will be executed when the transmission process is considered completed,
+ *       even if it was not succesful.
+ *       The transmission process is considered completed when the APS ACK is received (transmission succesful)
+ *       or when the maximum number of attempts has been reached.
+ *       If there is not ACK at APS layer it will retransmit once, and if there is not ACK at MAC level,
+ *       it will retransmit up to three times. But the callback function is not executed on every attempt,
+ *       it is executed only when the process is considered completed.
  *
  */
-void user_data_tx_status(zb_bufid_t bufid)
+void user_data_tx_status_cb(zb_bufid_t bufid)
 {
     if( bufid )
     {
+        zb_uint8_t *pointerToBeginOfBuffer;
+        zb_uint8_t aps_counter;
+        zb_uint8_t nwk_sequence_number;
+        zb_uint8_t mac_sequence_number;
+
+        pointerToBeginOfBuffer = zb_buf_begin(bufid);
+        pointerToBeginOfBuffer = pointerToBeginOfBuffer - 17;
+
+        if(PRINT_ZIGBEE_INFO)
+        {
+            mac_sequence_number = pointerToBeginOfBuffer[2];
+            nwk_sequence_number = pointerToBeginOfBuffer[16];
+            aps_counter = pointerToBeginOfBuffer[24];
+            LOG_DBG("Transmission completed, MAC seq = %d, NWK seq = %d, APS counter = %d", mac_sequence_number, nwk_sequence_number, aps_counter);
+        }
         zb_buf_free(bufid);
     }
 }
 
 //------------------------------------------------------------------------------
-/**@brief Callback to call when AF got APS packet.
+/**@brief Callback function excuted when AF gets APS packet.
  *
  * @param[in]   bufid   Reference to the Zigbee stack buffer used to pass signal.
- *                      
+ *
  */
 zb_uint8_t data_indication(zb_bufid_t bufid)
 {
- 	zb_apsde_data_indication_t *ind = ZB_BUF_GET_PARAM(bufid, zb_apsde_data_indication_t);  // Get APS header
-
     if (bufid)
 	{
+        zb_apsde_data_indication_t *ind = ZB_BUF_GET_PARAM(bufid, zb_apsde_data_indication_t);  // Get APS header
+
+        zb_uint8_t *pointerToBeginOfBuffer;
+        zb_uint8_t *pointerToEndOfBuffer;
+        zb_int32_t sizeOfPayload;
+        pointerToBeginOfBuffer = zb_buf_begin(bufid);
+        pointerToEndOfBuffer = zb_buf_end(bufid);
+        sizeOfPayload = pointerToEndOfBuffer - pointerToBeginOfBuffer;
+
+        if(PRINT_ZIGBEE_INFO) LOG_DBG("Rx APS Frame with profile 0x%x, cluster 0x%x, src_ep %d, dest_ep %d, payload %d bytes",
+                          (uint16_t)ind->profileid, (uint16_t)ind->clusterid, (uint8_t)ind->src_endpoint,
+                                                    (uint8_t)ind->dst_endpoint, (uint16_t)sizeOfPayload);
+
+        aps_frames_received_total_counter++;
+        if( ind->clusterid == DIGI_BINARY_VALUE_CLUSTER )aps_frames_received_binary_cluster_counter++;
+        if( ind->clusterid == DIGI_COMMISSIONING_CLUSTER )aps_frames_received_commissioning_cluster_counter++;
+
         if( (ind->clusterid == DIGI_BINARY_VALUE_CLUSTER) &&
             ( ind->src_endpoint == DIGI_BINARY_VALUE_SOURCE_ENDPOINT ) &&
             ( ind->dst_endpoint == DIGI_BINARY_VALUE_DESTINATION_ENDPOINT ) )
         {
-            zb_uint8_t *pointerToEndOfBuffer;
-            zb_int32_t sizeOfPayload;
-            pointerToBeginOfBuffer = zb_buf_begin(bufid);
-            pointerToEndOfBuffer = zb_buf_end(bufid);
-            sizeOfPayload = pointerToEndOfBuffer - pointerToBeginOfBuffer;
-
             if ((sizeOfPayload > 0) && (sizeOfPayload < UART_RX_BUFFER_SIZE))
             {
-                correct_RF_packets_received_counter ++;
-                if(PRINT_ZIGBEE_INFO)
+                /*if(PRINT_ZIGBEE_INFO)
                 {
                     LOG_DBG("Size of received payload is %d bytes \n", sizeOfPayload);
                     LOG_HEXDUMP_DBG(pointerToBeginOfBuffer,sizeOfPayload,"Payload of input RF packet");
-                    //LOG_DBG("\n Frame control field: %d \n", pointerToBeginOfBuffer[0]);
-                    //LOG_DBG("Sequence number: %d - \n", pointerToBeginOfBuffer[1]);
-                    //LOG_DBG("Zigbee Command: 0x%02x - \n", pointerToBeginOfBuffer[2]);
-                    //LOG_DBG("ind APS counter %d \n", ind->aps_counter);
-                }
+                }*/
 
                 if( !is_tcu_uart_in_command_mode() && (sizeOfPayload >= MODBUS_MIN_RX_LENGTH) )
                 {
-                    if(PRINT_ZIGBEE_INFO) LOG_DBG("Payload of input RF packet sent to Tcu UART \n");
+                    if(PRINT_ZIGBEE_INFO) LOG_DBG("Payload of input RF packet sent to TCU UART");
+                    tcu_uart_frames_transmitted_counter++;
                     sendFrameToTcu((uint8_t *)pointerToBeginOfBuffer, sizeOfPayload);
                 }
             }
@@ -247,21 +278,16 @@ zb_uint8_t data_indication(zb_bufid_t bufid)
             ( ind->src_endpoint == DIGI_COMMISSIONING_SOURCE_ENDPOINT ) &&
             ( ind->dst_endpoint == DIGI_COMMISSIONING_DESTINATION_ENDPOINT ) )
         {
-            zb_uint8_t *pointerToEndOfBuffer;
-            zb_int32_t sizeOfPayload;
-            pointerToBeginOfBuffer = zb_buf_begin(bufid);
-            pointerToEndOfBuffer = zb_buf_end(bufid);
-            sizeOfPayload = pointerToEndOfBuffer - pointerToBeginOfBuffer;
             if( is_a_digi_node_discovery_request((uint8_t *)pointerToBeginOfBuffer, (uint16_t)sizeOfPayload) )
             {
-                LOG_DBG("Xbee Node Discovery Device Request");
+                if(PRINT_ZIGBEE_INFO) LOG_DBG("Xbee Node Discovery Device Request");
             }
         }
 	}
 
     if (bufid)
     {
-		zb_buf_free(bufid); // JESUS: I don't know if this is needed, but I try, just in case.
+		zb_buf_free(bufid); // TODO: Should we free the buffer always? Or only in the cases where is processed as above?
 		return ZB_TRUE;
 	}
 	return ZB_FALSE;
@@ -316,8 +342,8 @@ void zboss_signal_handler(zb_bufid_t bufid)
 //------------------------------------------------------------------------------
 /**@brief Zigbee helper function  to send user payload
  *
- * @param[in]   outputPayload   Reference to the user payload 
- * @param[in]   chunk_size   payload size to send 
+ * @param[in]   outputPayload   Reference to the user payload
+ * @param[in]   chunk_size   payload size to send
  *
  */
 void send_user_payload(zb_uint8_t *outputPayload ,size_t chunk_size)
@@ -335,11 +361,11 @@ void send_user_payload(zb_uint8_t *outputPayload ,size_t chunk_size)
         zb_addr_u dst_addr;
         dst_addr.addr_short = 0x0000; // Destination address of the coordinator
 
-        if(PRINT_UART_INFO)
+        /*if(PRINT_UART_INFO)
         {
             LOG_DBG("send_user_payload: chunk_size %d\n", chunk_size);
             LOG_HEXDUMP_DBG(outputPayload,chunk_size,"Payload of output RF packet");
-        }
+        }*/
 
         zb_ret_t ret = zb_aps_send_user_payload(bufid,
                                                 dst_addr,
@@ -352,11 +378,17 @@ void send_user_payload(zb_uint8_t *outputPayload ,size_t chunk_size)
                                                 outputPayload,
                                                 chunk_size);
 
-        if(ret == RET_OK) LOG_DBG("Transmission was successful scheduled");
-        else if(ret == RET_INVALID_PARAMETER_1) LOG_ERR("Transmission could not be scheduled: The buffer is invalid");
-        else if(ret == RET_INVALID_PARAMETER_2) LOG_ERR("Transmission could not be scheduled: The payload_ptr parameter is invalid");
-        else if(ret == RET_INVALID_PARAMETER_3) LOG_ERR("Transmission could not be scheduled: The payload_size parameter is too large");
-        else LOG_ERR("Unkown error zb_aps_send_user_payload");
+        if(PRINT_ZIGBEE_INFO)
+        {
+            if(ret == RET_OK)
+            {
+                LOG_DBG("Scheduled APS Frame with cluster 0x%x and payload %d bytes", DIGI_BINARY_VALUE_CLUSTER, (uint16_t)chunk_size);
+            }
+            else if(ret == RET_INVALID_PARAMETER_1) LOG_ERR("Transmission could not be scheduled: The buffer is invalid");
+            else if(ret == RET_INVALID_PARAMETER_2) LOG_ERR("Transmission could not be scheduled: The payload_ptr parameter is invalid");
+            else if(ret == RET_INVALID_PARAMETER_3) LOG_ERR("Transmission could not be scheduled: The payload_size parameter is too large");
+            else LOG_ERR("Unkown error zb_aps_send_user_payload");
+        }
     }
     else
     {
@@ -443,7 +475,6 @@ static void timer1_init(void)
 }
 
 //------------------------------------------------------------------------------
-
 /**@brief Function for initializing the GPIO pins.
  *        Currently, only one pin is initialized. Configured as output to drive a led.
  * @retval -1 Error
@@ -493,7 +524,12 @@ void zigbee_configuration()
 	zb_bdb_set_legacy_device_support(ZB_TRUE);
 }
 
-// Function for giagnostic purposes. toogle the diagnostic led every second to be sure HW is OK and app is running
+//------------------------------------------------------------------------------
+/**@brief This function toggles and output pin at 1Hz. That output pin is connected to a LED
+ *
+ * @note Executed the in main loop
+ *
+ */
 void diagnostic_toogle_pin()
 {
     if(debug_led_ms_x10 >= 10000)
@@ -503,7 +539,15 @@ void diagnostic_toogle_pin()
     }
 }
 
-// Function for giagnostic purposes. Print zigbee info when the device joins a network
+//------------------------------------------------------------------------------
+/**@brief This function prints the value of several Zigbee parameters to the console.
+ *
+ *
+ * @details Information is printed only once, after the device has joined a Network
+ *
+ * @note Executed the in main loop
+ *
+ */
 void diagnostic_zigbee_info()
 {
     zb_ieee_addr_t zb_long_address;
@@ -561,21 +605,37 @@ void diagnostic_zigbee_info()
     }
 }
 
+//------------------------------------------------------------------------------
+/**@brief This function prints the contents of multiple counters to the console.
+ *
+ *
+ * @details Information is printed once per minute. Those counters contain information about
+ *        the number of RF packets received, RF packets schedule for transmission,...
+ *
+ * @note Executed in the main loop
+ *
+ */
 void display_counters(void)
 {
-    if(correct_RF_packets_received_counter != correct_RF_packets_received_counter_old)
+    static uint64_t time_last_ms = 0;
+    uint64_t time_now_ms = k_uptime_get();
+    if( (uint64_t)( time_now_ms - time_last_ms ) > 60000 )
     {
-        correct_RF_packets_received_counter_old = correct_RF_packets_received_counter;
-        printk("Input counter: %d \n", correct_RF_packets_received_counter);
-    }
-
-    if(tcu_uart_frames_received_counter != tcu_uart_frames_received_counter_old)
-    {
-        tcu_uart_frames_received_counter_old = tcu_uart_frames_received_counter;
-        printk("Output counter: %d \n", tcu_uart_frames_received_counter);
+        time_last_ms = time_now_ms;
+        LOG_DBG("APS RX COUNTERS: Total %d, Binary %d, Commis %d",
+                               aps_frames_received_total_counter,
+                               aps_frames_received_binary_cluster_counter,
+                               aps_frames_received_commissioning_cluster_counter);
+        LOG_DBG("Uart frames: Tx %d, Rx %d",
+                               tcu_uart_frames_transmitted_counter,
+                               tcu_uart_frames_received_counter);
     }
 }
 
+//------------------------------------------------------------------------------
+/**@brief Main function
+ *
+ */
 int main(void)
 {
     LOG_INF("Router started successfully");
@@ -605,7 +665,7 @@ int main(void)
     zigbee_configuration(); //Zigbee configuration
     zigbee_enable(); // Start Zigbee default thread
     zb_af_set_data_indication(data_indication); // Set call back function for APS frame received
-    zb_aps_set_user_data_tx_cb(user_data_tx_status); // Set call back function for APS frame transmitted
+    zb_aps_set_user_data_tx_cb(user_data_tx_status_cb); // Set call back function for APS frame transmitted
     while(1)
     {
         // run diagnostic functions
